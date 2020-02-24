6a806bb57a7272a04e6e0a30b345922f7eff7108
Cain <xu.chen28@qq.com>
2020-02-24 17:36:41 +0800

    Daily Update

--- playgrounds/playground.js
+++ playgrounds/playground.js
@@ -19,5 +19,42 @@ console.log(3 == 2+1)
 var str = '\8'
 console.log(str)
 
+var obj = {
+    'f' : 'a',
+    'c' : 'b',
+    1:3,
+    p:function (x) {
+        return x**2
+    }
+}
+console.log( Object.keys(obj)[2])
+
+var pr = function x() {
+    console.log("pr")
+    pr(), x()
+}
+
+var add = new Function(
+    'x',
+    'y',
+    'return x + y'
+);
+
+console.log(add.length)
+
+
+logNum = 233333
+console.log(logNum)
+var logNum
+
+
+(function () {
+    console.log("立即执行函数")
+}())
+var evalPara = eval('console.log("eval试验")');
+console.log( typeof console.log(typeof eval(123)))
+
+
+
 
 
